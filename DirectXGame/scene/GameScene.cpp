3cd38e02d5ddd586ae23caab673586497d8c3dc0
satsuki329 <k021g1414@g.neec.ac.jp>
2024-06-06 15:52:18 +0900

    2-2終わり

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -26,7 +26,7 @@ void GameScene::Initialize() {
 	audio_ = Audio::GetInstance();
 
 	// ファイル名を指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("bed.png");
+	//textureHandle_ = TextureManager::Load("bed.png");
 	// 3Dモデルの生成
 	model_ = Model::Create();
 	modelBlock_ = Model::Create();
@@ -96,7 +96,8 @@ void GameScene::Update() {
 	}
 
 	// 自キャラの更新
-	player_->Update();
+	//player_->Update();
+
 
 	// 縦横ブロック更新
 	for (std::vector<WorldTransform*> worldTransformBlockTate : worldTransformBlocks_) {
