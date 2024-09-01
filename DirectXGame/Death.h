d79dec7f4de2f5d58e1c534f4e75776871fbb2d3
satsuki329 <k021g1414@g.neec.ac.jp>
2024-09-01 20:08:23 +0900

    hozon

--- DirectXGame/Death.h
+++ DirectXGame/Death.h
@@ -16,15 +16,15 @@ public:
 
 private:
 	static inline const uint32_t kNumParticles = 8;
-	//static inline const float kDuration = 2.0f;
-	//static inline const float kSpeed = 0.05f;
-	//static inline const float kAngleUnit = 2.0f * std::numbers::pi_v<float> / kNumParticles;
+	// static inline const float kDuration = 2.0f;
+	// static inline const float kSpeed = 0.05f;
+	// static inline const float kAngleUnit = 2.0f * std::numbers::pi_v<float> / kNumParticles;
 
 	Model* model_ = nullptr;
 	std::array<WorldTransform, kNumParticles> worldtransforms_;
 	ViewProjection* viewProjection_ = nullptr;
-	//bool isFinished_ = false;
-	//float counter_ = 0.0f;
-	//ObjectColor objectColor_;
-	//Vector4 color_;
+	// bool isFinished_ = false;
+	// float counter_ = 0.0f;
+	// ObjectColor objectColor_;
+	// Vector4 color_;
 };
